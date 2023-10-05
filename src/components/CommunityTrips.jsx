7cb496a3c7e90c9339a04fbb5178569b7d2e6f74
aditya <adiraiar022@gmail.com>
2023-10-05 19:41:08 +0530

    added Communitytrip fucntionality through api

--- src/components/CommunityTrips.jsx
+++ src/components/CommunityTrips.jsx
@@ -1,15 +1,70 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import SideFilterBar from "./SideFilterBar";
 import Trip from "./trip";
+import BottomNavbar from "./BottomNavbar";
+import {getCookieValue} from './cookieFunc'
 
 const CommunityTrips = () => {
+
+    const [filterData, setFilterData] = useState({
+        origin:'',
+        destination:'',
+        date:'',
+        timeRangeStartTime:'',
+        timeRangeEndTime:''
+    })
+
+    const handleFilterData = (value) => {
+        setFilterData(value);
+    }
+
+    const { origin, destination, date, timeRangeEndTime, timeRangeStartTime } = filterData;
+
+    const getCommunityTripsUrl = `http://localhost:4000/communities/5/trips?origin=${origin}&destination=${destination}&date=${date}&timeRangeStartTime${timeRangeStartTime}&timeRangeEndTime=${timeRangeEndTime}`;
+    const [trips, setTrips] = useState([]);
+
+    const authToken = getCookieValue(document.cookie, 'authtoken');
+
+    useEffect(()=>{
+        fetchAllTrips();
+    },[])
+
+    const fetchAllTrips = async () =>{
+        try {
+            const response = await fetch(getCommunityTripsUrl,{
+                method:'GET',
+                headers:{
+                    'auth-token':authToken,
+                    'Content-Type':'application/json',
+                }
+            })
+
+            if(response.ok)
+            {
+                const json = await response.json();
+                setTrips(json.results);
+            }
+        } catch (error) {
+            console.log("error while fetching the trips:",error);
+        }
+    }
+
     return(
         <>
         <div className="mainCommunityTripContainer">
-            <SideFilterBar/>
+            <SideFilterBar handleFilterData = {handleFilterData} fetchAllTrips = {fetchAllTrips}/>
             <h3>Community Trips</h3>
-            <Trip/>
+            <div style={{marginBottom:'4vw'}}>
+          {trips.map((ele) => {
+            return <div style={{marginBottom:'2.5vh'}} key={"AllTripsId_"+ele.trip_id}>
+              <Trip origin = {ele.trip_origin} destination = {ele.trip_destination} desc = {ele.trip_desc} arrival = {ele.trip_arrival_datetime} departure = {ele.trip_departure_datetime}/>
+            </div>
+          })}
+          </div>
         </div>
+        <footer className="footer">
+        <BottomNavbar/>
+        </footer>
         </>
     )
 }
